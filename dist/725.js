66e1ff68d0a647acf2b06515352072a1a9a61e34
andres espino <aespino1290@gmail.com>
2023-07-09 18:11:58 +0200

    feat: use runtime global

--- dist/725.js
+++ dist/725.js
@@ -1,2 +1,2 @@
-(self.webpackChunkchunk_loading_missing=self.webpackChunkchunk_loading_missing||[]).push([[725],{725:()=>{console.log("bad chunk")}});
+(self.webpackChunkchunk_loading_missing=self.webpackChunkchunk_loading_missing||[]).push([[725],{725:()=>{console.log("bad chunk")}]);
 //# sourceMappingURL=725.js.map
